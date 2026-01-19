c7ad3086e298dd9c41e663697eaee235521774b5
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-19 22:34:34 +0100

    Added Swagger

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Message Api",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Message Api");
+    c.RoutePrefix = string.Empty;
+});
+
+app.Run();
